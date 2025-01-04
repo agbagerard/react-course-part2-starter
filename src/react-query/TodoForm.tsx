831cfe18b02ad9b7c94c36689579367f15698448
Denakpo Agba <agbagerard@gmail.com>
2025-01-04 09:37:56 -0500

    Showing mutation progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -11,18 +11,21 @@ const TodoForm = () => {
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
-    onSuccess: (savedTodo, newTodo) => {
+    onSuccess: (savedTodo) => {
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
         savedTodo,
         ...(todos || []),
       ]);
+      if(ref.current) ref.current.value = "";
     },
   });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
     <>
-      {addTodo.error && <div className="alert alert-danger">{addTodo.error.message}</div>}
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
       <form
         className="row mb-3"
         onSubmit={(event) => {
@@ -41,7 +44,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button disabled={addTodo.isLoading} className="btn btn-primary">
+            {addTodo.isLoading ? "Adding..." : "Add"}
+          </button>
         </div>
       </form>
     </>
